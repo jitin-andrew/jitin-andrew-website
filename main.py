8310d110ddee28491159ef4da6de3cf2adc81c1e
jitin-andrew <132670226+jitin-andrew@users.noreply.github.com>
2023-05-07 05:46:26 +0000

    I have changed the website name

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def andrew():
-  return ("hello, this is andrew!")
+
+def careers():
+  return "Hello Developers!"
+
 if __name__ == "__main__":
-  app.run(host = "0.0.0.0", debug = True)
+  app.run(host= "0.0.0.0",debug = True)
