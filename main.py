ed6393a571fb001b588795ca100218a351842973
jitin-andrew <132670226+jitin-andrew@users.noreply.github.com>
2023-05-07 07:53:03 +0000

    Updated html file

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
 
+@app.route("/")
 def careers():
-  return "Hello Developers!"
+  return render_template("homepage.html")
+
 
 if __name__ == "__main__":
-  app.run(host= "0.0.0.0",debug = True)
+  app.run(host="0.0.0.0", debug=True)
